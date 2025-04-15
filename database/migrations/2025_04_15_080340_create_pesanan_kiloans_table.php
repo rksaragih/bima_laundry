45af7ab315fa5fffb5468e6dee4b552570df4f3e
rksaragih <168797432+rksaragih@users.noreply.github.com>
2025-04-15 18:21:18 +0700

    Database Migration

---
+++ database/migrations/2025_04_15_080340_create_pesanan_kiloans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesanan_kiloans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('users')->onDelete('cascade');
+            $table->foreignId('id_pelanggan')->constrained('pelanggans')->onDelete('cascade');
+            $table->foreignId('id_layanan')->constrained('layanans')->onDelete('cascade');
+            $table->string('jenis_barang');
+            $table->text('spesifikasi_barang');
+            $table->integer('berat_pakaian');
+            $table->string('status_cucian');
+            $table->string('status_pembayaran');
+            $table->integer('total_harga');
+            $table->date('tanggal_terima');
+            $table->date('tanggal_selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesanan_kiloans');
+    }
+};
