c93f5f8440d2151277a92efde6bddbc2492c1408
Davino Rizqy Dayan <vino.raisya@gmail.com>
2025-04-17 10:16:50 +0700

    modified user models and migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('username')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
